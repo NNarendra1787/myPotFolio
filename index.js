9fd51b7af1f9ba29aca4956d60b3a7f3b38f6e15
Narendra <narendranarwade2002@gmail.com>
2024-03-09 21:19:00 +0530

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+/*===  = == = typing animation = ===== */
+var typed = new Typed(".typing",{
+    strings: ["", "UI Designer", "Web Devloper", "React Devloper", "MERN-Stack Devloper", "Full-Stack Devloper"],
+    typeSpeed: 150,
+    backSpeed: 60,
+    loop: true
+
+})
